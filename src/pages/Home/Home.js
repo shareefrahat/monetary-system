68058833606bc01680abf5e011a338260442b078
ShareefRahat <imr4h47@gmail.com>
2022-06-11 15:58:05 +0600

    section 3 is ready

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import avatar from "../../images/user-7.jpg";
 import GraphSection from "./GraphSection";
 
@@ -7,8 +8,8 @@ const Home = () => {
   const [age, setAge] = useState(100);
   return (
     <>
-      <div className="flex flex-col lg:flex-row gap-10">
-        <section className="bg-gray-50/80 lg:pl-32 lg:pr-20 px-6 py-10 h-screen w-full lg:w-1/4">
+      <div className="flex flex-col lg:flex-row justify-between gap-10">
+        <section className="bg-gray-50/80 lg:pl-32 lg:pr-20 px-6 py-10 h-screen">
           <div className="flex flex-row justify-start text-left gap-4 items-center">
             <img className="w-20 rounded-full" src={avatar} alt="" />
             <p className="text-xl lg:text-2xl font-bold">
@@ -33,9 +34,9 @@ const Home = () => {
               <h3 className="text-lg lg:text-xl font-bold">$1,892</h3>
               <small className="text-sm text-neutral">Total Interest</small>
             </div>
-            <button className="btnbtn px-6 py-2 bg-primary text-slate-100 rounded-md">
+            <button className="btn px-6 py-3 bg-primary text-slate-100 rounded-xl">
               <select
-                className=" bg-primary text-slate-100 rounded-md"
+                className=" bg-primary text-slate-100 rounded-xl outline-none"
                 name="action"
                 id="user-action"
               >
@@ -61,12 +62,12 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <section className="lg:px-20 px-6 py-10 w-full lg:w-2/4">
+        <section className="lg:px-16 px-6 py-10 ">
           <GraphSection></GraphSection>
         </section>
-        <section className="lg:px-20 px-6 py-10 mb-20 lg:mb-0 w-full lg:w-1/4">
+        <section className="lg:px-16 px-6 py-10 mb-20 lg:mb-0">
           <div>
-            <div className="bg-gray-50/80 p-6 rounded-md">
+            <div className="bg-gray-50/80 p-6 rounded-md mb-10">
               <div className="border-b border-b-slate-200 mb-4">
                 <p className="text-lg mb-6 font-bold">Retirement Strategy</p>
                 <div className="mb-6">
@@ -102,9 +103,41 @@ const Home = () => {
                   </p>
                 </div>
               </div>
-              <div></div>
+              <div className="mt-10">
+                <p className="text-sm mb-5 flex flex-row justify-between items-center">
+                  <span>Employer Contribution</span>
+                  <span>8.4%</span>
+                </p>
+                <p className="text-sm mb-5 flex flex-row justify-between items-center">
+                  <span>Interest Rate</span>
+                  <span>5%</span>
+                </p>
+                <button className="btn px-6 py-3 bg-primary text-slate-100 rounded-xl w-full">
+                  Update
+                </button>
+                <Link
+                  to="/"
+                  className="text-sm text-center text-primary mt-5 block"
+                >
+                  View Help Docs {">"}
+                </Link>
+              </div>
             </div>
-            <div></div>
+            <div>
+              <div className="border-l-2 border-l-primary py-1 px-8">
+                <p className="text-md mb-2">
+                  <span>Are you considering a</span>
+                  <br />
+                  <span className="font-bold">Housing Advance?</span>
+                </p>
+                <small className="text-neutral block">
+                  Limited time reduced interest
+                </small>
+                <Link to="/" className="text-sm text-primary mt-2 block">
+                  Learn More {">"}
+                </Link>
+              </div>
+            </div>
           </div>
         </section>
       </div>
